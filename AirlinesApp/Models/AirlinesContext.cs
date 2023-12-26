7772028ab6f4b1bd38909a43ecc3befe27b1e46d
Egorrr3 <egorrr170@gmail.com>
2023-12-26 22:44:01 +0200

    commit

--- AirlinesApp/Models/AirlinesContext.cs
+++ AirlinesApp/Models/AirlinesContext.cs
@@ -25,8 +25,6 @@ public partial class AirlinesContext : DbContext
 
     public virtual DbSet<Ticket> Tickets { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-97Q88N1\\SQLEXPRESS;Database=Airlines; Trusted_Connection=True; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- AirlinesApp/RabbitMQ/RabbitMQService.cs
+++ AirlinesApp/RabbitMQ/RabbitMQService.cs
@@ -14,23 +14,27 @@ namespace RestaurantWebApplication.RabbitMQ
 
         public void SendMessage(string message)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            try
             {
-                channel.QueueDeclare(queue: "Q",
-                               durable: false,
-                               exclusive: false,
-                               autoDelete: false,
-                               arguments: null);
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Q",
+                                   durable: false,
+                                   exclusive: false,
+                                   autoDelete: false,
+                                   arguments: null);
 
-                var body = Encoding.UTF8.GetBytes(message);
+                    var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(exchange: "",
-                               routingKey: "Q",
-                               basicProperties: null,
-                               body: body);
+                    channel.BasicPublish(exchange: "",
+                                   routingKey: "Q",
+                                   basicProperties: null,
+                                   body: body);
+                }
             }
+            catch (Exception ex) { }
         }
     }
 }
